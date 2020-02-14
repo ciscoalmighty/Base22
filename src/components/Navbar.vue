dcc8dec65347a5d8ac0052705ae2ae9f4cce5819
ciscoalmighty <50853679+ciscoalmighty@users.noreply.github.com>
2020-02-13 20:15:35 -0500

    first push

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+      <!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Bootstrap
+  </a>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
