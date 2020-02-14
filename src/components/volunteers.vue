039012136169f95197f42f1a9b84594715516f9a
ciscoalmighty <50853679+ciscoalmighty@users.noreply.github.com>
2020-02-13 21:01:22 -0500

    update

---
+++ src/components/volunteers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+      <!-- Image and text -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'volunteers',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
